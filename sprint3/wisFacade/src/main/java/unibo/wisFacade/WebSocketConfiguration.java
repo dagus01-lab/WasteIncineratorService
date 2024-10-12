bb50c849aa863e27ed1c192330020e1b94100fc5
dagus01-lab <gabriele.daga@studio.unibo.it>
2024-10-12 17:25:34 +0200

    sprint 3: refactor

--- sprint3/wisFacade/src/main/java/unibo/wisFacade/WebSocketConfiguration.java
+++ sprint3/wisFacade/src/main/java/unibo/wisFacade/WebSocketConfiguration.java
@@ -1,17 +1,23 @@
 package unibo.wisFacade;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import unibo.basicomm23.utils.CommUtils;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
-public static final WebSocketHandler wshandler = new WebSocketHandler();
-public static final String wspath                  = "socket";
-  @Override
-  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
-    registry.addHandler(wshandler, wspath).setAllowedOrigins("*");
-  }
+
+    public final String wsPath  = "accessgui";
+
+    public WebSocketConfiguration(){
+        //Inovocato alla CONNESSIONE
+        //CommUtils.outred("WebSocketConfiguration onnnnnnnnnnnnnnnnnn");
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(FacadeBuilder.wsHandler, wsPath).setAllowedOrigins("*");
+    }
 }
