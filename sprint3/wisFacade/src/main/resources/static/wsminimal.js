a619e361eda8b593d68193e75044563ae9b72a28
dagus01-lab <gabriele.daga@studio.unibo.it>
2024-10-16 18:51:52 +0200

    sprint3: fixed websocket frontend and updated mqtt message handling in wis

--- sprint3/wisFacade/src/main/resources/static/wsminimal.js
+++ sprint3/wisFacade/src/main/resources/static/wsminimal.js
@@ -3,39 +3,24 @@ var socket = connect();
 function connect(){
   var host     = document.location.host;
   var pathname =  document.location.pathname;
-  var addr     = "ws://" +host + pathname + "socket"  ;
+  var addr     = "ws://" +host + pathname +"accessgui"  ;
 
   // Assicura che sia aperta un unica connessione
   if(socket!==undefined && socket.readyState!==WebSocket.CLOSED){
     alert("WARNING: Connessione WebSocket già stabilita");
   }
   socket = new WebSocket(addr); //CONNESSIONE
-
+  socket.onerror = function (event) {
+      console.error("WebSocket error observed:", event);
+  };
   socket.onmessage = function (event) { //RICEZIONE
     updateGUI(""+`${event.data}`);
   };
   return socket;
 }//connect
 
-const incineratorStatus = document.getElementById("incineratorStatus")
-const ashStorageStatus = document.getElementById("ashStorageStatus")
-const opRobotStatus = document.getElementById("opRobotStatus")
-const wasteStoragePackets = document.getElementById("wasteStoragePackets")
-
 function updateGUI(message) {
-	if(message.includes("wasteStorageStatus")){
-		wasteStorageStatus.innerHTML = message.split("(")[1].split(")")[0]
-	}
-	else if(message.includes("ashStorageStatus")){
-		ashStorageStatus.innerHTML = message.split("(")[1].split(")")[0]
-	}
-	else if(message.includes("opRobotStatus")){
-		opRobotStatus.innerHTML = message.split("(")[1].split(")")[0]
-	}
-	else if(message.includes("wasteStoragePackets")){
-		wasteStoragePackets.innerHTML = message.split("(")[1].split(")")[0]
-	}
-	else{
-		alert("Unknown message: "+message)
-	}
+	payload = message.split("(")[1].split(")")[0]
+	element = message.split("(")[0]
+	document.getElementById(element).innerHTML = payload
 }
