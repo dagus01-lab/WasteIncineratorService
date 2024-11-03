da81dd6eb2909a2e815a1b987754a6510a7d48f2
dagus01-lab <gabriele.daga@studio.unibo.it>
2024-11-04 00:45:54 +0100

    sprint4: added deployment with raspberry pi. TODO: optimize resource usage in services that have to run on raspberry pi

--- sprint4/deployment/deploywis.sh
+++ sprint4/deployment/deploywis.sh
@@ -1,27 +1,32 @@
 #first you have to ensure swap is disabled:
 #sudo swapoff -a
 #then you can start the kubeadm daemon:
-#sudo kubeadm init ----config=kubeadm_config.yaml
-#join on the two raspberry
+#curl -sfL https://get.k3s.io | sh -
+
+#after installing it for the first time, you can enable/disable it just with:
+#sudo systemctl start k3s
+#or
+#sudo systemctl stop k3s
+
+#join on the two raspberry by retrieving the Node Token and Master IP Address On the master node, get the node token, 
+#which will be used for workers to join the cluster:
+#cat /var/lib/rancher/k3s/server/node-token
+#and then join on the raspberry devices:
+#curl -sfL https://get.k3s.io | K3S_URL=https://<master-ip>:6443 K3S_TOKEN=<node-token> sh -
+
+#after installing it for the first time, you can first copy from the manager node the configuration file:
+#sudo k3s agent --server https://<control-plane-ip>:6443 --token <node-token>
+
 
 #in order to be able to access to the cluster with kubectl, run:
-#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
 #sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-#if not installed, you need to install a CNI plugin to be able to manage the network:
-#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+#with k3s there is no need to install a CNI plugin, as we have flannel already installed
 
 #then it is important to remove the taint that prevents scheduling (substitute the manager-node name with ther right name!!):
 #kubectl taint nodes <manager-node> node-role.kubernetes.io/control-plane:NoSchedule-
 
-# kubectl create secret generic stunnel-secrets \
-#   --from-file=certs/client.crt \
-#   --from-file=certs/client.key \
-#   --from-file=certs/ca.crt \
-#   --from-file=certs/basicrobot.key \
-#   --from-file=certs/basicrobot.crt \
-#   --from-file=certs/server.crt \
-#   --from-file=certs/server.key
 kubectl delete configmap config-files
 kubectl create configmap config-files \
   --from-file=configs/stunnel_wis.conf \
